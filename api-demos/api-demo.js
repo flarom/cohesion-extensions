6b36df0f6003bf8dfed2f125d6f1d3fdc5d1e204
flarom <flarocontato@gmail.com>
2026-03-01 18:53:36 -0300

    test commit

--- api-demos/api-demo.js
+++ api-demos/api-demo.js
@@ -2,9 +2,9 @@
  * @extension
  * @title Example Extension
  * @author Flarom
- * @version 1.0.0
+ * @version 1.0.1
  * @updateLink https://github.com/flarom/cohesion-extensions/blob/main/api-demos/api-demo.js
  * @description Example
  */
 
-console.log("Hello world");
+console.log("Hello world, this is a test");
